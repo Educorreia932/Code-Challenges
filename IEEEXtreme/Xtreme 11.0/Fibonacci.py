dca9ef4b1eadbc7098a6a2cb59928b7452677a2b
Educorreia932 <skelozard1@gmail.com>
2020-10-19 23:17:02 +0100

    IEEEXtreme 11.0 - Fibonacci done

---
+++ IEEEXtreme/Xtreme 11.0/Fibonacci.py
@@ -0,0 +1,23 @@
+# Number of test cases
+t = int(input())
+
+def fibonacci(n):
+    a, b = 1, 1
+
+    for j in range(n - 1):
+        a, b = a + b, a
+    
+    return a
+
+last_digits = []
+
+# The last digit of a fibonacci sequence number repeats every 60th term
+for i in range(60):
+    last_digit = str(fibonacci(i))[-1]
+    last_digits.append(last_digit) 
+
+for i in range(t):
+    m = int(input())
+
+    print(last_digits[m % 60])
+
