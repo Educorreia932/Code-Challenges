442b45ad4419d5f65407ae81c90d93df3553114e
Educorreia932 <skelozard1@gmail.com>
2021-08-05 15:23:26 +0100

    Project Euler - Problem 56 done

---
+++ Project Euler/52.py
@@ -0,0 +1,15 @@
+n = 1
+
+while True:
+    digits = set(int(x) for x in str(n))
+
+    for i in range(2, 7):
+        if set(int(x) for x in str(n * i)) != digits:
+            break
+
+    else:
+        print(n)
+        break
+
+    n += 1
+    
