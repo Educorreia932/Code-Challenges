2a467df5e954e977133b28ff1ae71a6064f7f8f8
Eduardo Correia <skelozard1@gmail.com>
2021-12-01 14:39:10 +0000

    File name shenanigans

--- CodeWars/Haskell/altERnaTIng cAsE <=> ALTerNAtiNG CaSe.hs
+++
@@ -1,6 +0,0 @@
-module Codewars.Kata.AlternatingCase where  
-
-import Data.Char
-
-toAlternatingCase :: String -> String 
-toAlternatingCase = map (\c -> if isLower c then toUpper c; else toLower c)
