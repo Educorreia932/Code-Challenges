1566210d1fbc774de523a87fd09a61cfc3d057c4
Educorreia932 <skelozard1@gmail.com>
2022-12-02 21:48:42 +0900

    Advent of Code 2022 | Day 2 (part 2) done

--- Advent of Code/2022/Day 2/solution.c
+++ Advent of Code/2022/Day 2/solution.c
@@ -20,28 +20,47 @@ int mod(int a, int b) {
     return r < 0 ? r + b : r;
 }
 
+int calculate_score(int x, int y) {
+    int delta = y - x,
+        result = mod((delta + 1), 3) - 1,
+        shape_score = 3 * (result + 1),
+        outcome_score = y + 1;
+
+    return shape_score + outcome_score;
+}
+
 int part_one() {
     int total_score = 0;
 
     for (int i = 0; i < count; i += 2) {
-        int x = (rounds[i] - 'A'),
-            y = (rounds[i + 1] - 'X'),
-            delta = y - x,
-            result = mod((delta + 1), 3) - 1,
-            shape_score = 3 * (result + 1),
-            outcome_score = y + 1;
+        int opponent = (rounds[i] - 'A'),
+            player = (rounds[i + 1] - 'X');
 
-        total_score += shape_score + outcome_score;
+        total_score += calculate_score(opponent, player);
     }
 
     return total_score;
 }
 
+int part_two() {
+    int total_score = 0;
+
+    for (int i = 0; i < count; i += 2) {
+        int opponent = (rounds[i] - 'A'),
+            result = (rounds[i + 1] - 'X') - 1,
+            player = mod(opponent + result, 3);
+
+        total_score += calculate_score(opponent, player);
+    }
+
+    return total_score;
+}
 
 int main() {
     read_input();
 
     printf("Part One: %d\n", part_one());
+    printf("Part Two: %d\n", part_two());
 
     return 0;
 }
